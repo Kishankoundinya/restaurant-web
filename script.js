300d071998385a5af3b2b06e184f6de54cfedab8
Kishankoundinya <kishankoundinya31@gmail.com>
2025-01-12 18:44:33 +0530

    uploaded script.js

---
+++ script.js
@@ -0,0 +1,364 @@
+var price = 0;
+let tp = document.getElementById("tp");
+
+let flag1 = 0;
+item1.addEventListener("click", function () {
+    if (flag1 == 0) {
+        price += 599;
+        tp.innerHTML = price;
+        flag1 = 1;
+    } else {
+        price -= 599;
+        tp.innerHTML = price;
+        flag1 = 0;
+    }
+});
+
+let flag2 = 0;
+item2.addEventListener("click", function () {
+    if (flag2 == 0) {
+        price += 499;
+        tp.innerHTML = price;
+        flag2 = 1;
+    } else {
+        price -= 499;
+        tp.innerHTML = price;
+        flag2 = 0;
+    }
+});
+
+let flag3 = 0;
+item3.addEventListener("click", function () {
+    if (flag3 == 0) {
+        price += 699;
+        tp.innerHTML = price;
+        flag3 = 1;
+    } else {
+        price -= 699;
+        tp.innerHTML = price;
+        flag3 = 0;
+    }
+});
+
+let flag4 = 0;
+item4.addEventListener("click", function () {
+    if (flag4 == 0) {
+        price += 550;
+        tp.innerHTML = price;
+        flag4 = 1;
+    } else {
+        price -= 550;
+        tp.innerHTML = price;
+        flag4 = 0;
+    }
+});
+
+let flag5 = 0;
+item5.addEventListener("click", function () {
+    if (flag5 == 0) {
+        price += 450;
+        tp.innerHTML = price;
+        flag5 = 1;
+    } else {
+        price -= 450;
+        tp.innerHTML = price;
+        flag5 = 0;
+    }
+});
+
+let flag6 = 0;
+item6.addEventListener("click", function () {
+    if (flag6 == 0) {
+        price += 799;
+        tp.innerHTML = price;
+        flag6 = 1;
+    } else {
+        price -= 799;
+        tp.innerHTML = price;
+        flag6 = 0;
+    }
+});
+
+let flag7 = 0;
+item7.addEventListener("click", function () {
+    if (flag7 == 0) {
+        price += 350;
+        tp.innerHTML = price;
+        flag7 = 1;
+    } else {
+        price -= 350;
+        tp.innerHTML = price;
+        flag7 = 0;
+    }
+});
+
+let flag8 = 0;
+item8.addEventListener("click", function () {
+    if (flag8 == 0) {
+        price += 650;
+        tp.innerHTML = price;
+        flag8 = 1;
+    } else {
+        price -= 650;
+        tp.innerHTML = price;
+        flag8 = 0;
+    }
+});
+
+let flag9 = 0;
+item9.addEventListener("click", function () {
+    if (flag9 == 0) {
+        price += 750;
+        tp.innerHTML = price;
+        flag9 = 1;
+    } else {
+        price -= 750;
+        tp.innerHTML = price;
+        flag9 = 0;
+    }
+});
+
+let flag10 = 0;
+item10.addEventListener("click", function () {
+    if (flag10 == 0) {
+        price += 650;
+        tp.innerHTML = price;
+        flag10 = 1;
+    } else {
+        price -= 650;
+        tp.innerHTML = price;
+        flag10 = 0;
+    }
+});
+
+let flag11 = 0;
+item11.addEventListener("click", function () {
+    if (flag11 == 0) {
+        price += 699;
+        tp.innerHTML = price;
+        flag11 = 1;
+    } else {
+        price -= 699;
+        tp.innerHTML = price;
+        flag11 = 0;
+    }
+});
+
+let flag12 = 0;
+item12.addEventListener("click", function () {
+    if (flag12 == 0) {
+        price += 450;
+        tp.innerHTML = price;
+        flag12 = 1;
+    } else {
+        price -= 450;
+        tp.innerHTML = price;
+        flag12 = 0;
+    }
+});
+
+let flag13 = 0;
+item13.addEventListener("click", function () {
+    if (flag13 == 0) {
+        price += 199;
+        tp.innerHTML = price;
+        flag13 = 1;
+    } else {
+        price -= 199;
+        tp.innerHTML = price;
+        flag13 = 0;
+    }
+});
+
+let flag14 = 0;
+item14.addEventListener("click", function () {
+    if (flag14 == 0) {
+        price += 299;
+        tp.innerHTML = price;
+        flag14 = 1;
+    } else {
+        price -= 299;
+        tp.innerHTML = price;
+        flag14 = 0;
+    }
+});
+
+let flag15 = 0;
+item15.addEventListener("click", function () {
+    if (flag15 == 0) {
+        price += 600;
+        tp.innerHTML = price;
+        flag15 = 1;
+    } else {
+        price -= 600;
+        tp.innerHTML = price;
+        flag15 = 0;
+    }
+});
+
+let flag16 = 0;
+item16.addEventListener("click", function () {
+    if (flag16 == 0) {
+        price += 800;
+        tp.innerHTML = price;
+        flag16 = 1;
+    } else {
+        price -= 800;
+        tp.innerHTML = price;
+        flag16 = 0;
+    }
+});
+
+let flag17 = 0;
+item17.addEventListener("click", function () {
+    if (flag17 == 0) {
+        price += 750;
+        tp.innerHTML = price;
+        flag17 = 1;
+    } else {
+        price -= 750;
+        tp.innerHTML = price;
+        flag17 = 0;
+    }
+});
+
+let flag18 = 0;
+item18.addEventListener("click", function () {
+    if (flag18 == 0) {
+        price += 200;
+        tp.innerHTML = price;
+        flag18 = 1;
+    } else {
+        price -= 200;
+        tp.innerHTML = price;
+        flag18 = 0;
+    }
+});
+
+let flag19 = 0;
+item19.addEventListener("click", function () {
+    if (flag19 == 0) {
+        price += 350;
+        tp.innerHTML = price;
+        flag19 = 1;
+    } else {
+        price -= 350;
+        tp.innerHTML = price;
+        flag19 = 0;
+    }
+});
+
+let flag20 = 0;
+item20.addEventListener("click", function () {
+    if (flag20 == 0) {
+        price += 650;
+        tp.innerHTML = price;
+        flag20 = 1;
+    } else {
+        price -= 650;
+        tp.innerHTML = price;
+        flag20 = 0;
+    }
+});
+let flag21 = 0;
+item21.addEventListener("click", function () {
+    if (flag21 === 0) {
+        price += 300;
+        tp.innerHTML = price;
+        flag21 = 1;
+    } else {
+        price -= 300;
+        tp.innerHTML = price;
+        flag21 = 0;
+    }
+});
+
+let flag22 = 0;
+item22.addEventListener("click", function () {
+    if (flag22 === 0) {
+        price += 350;
+        tp.innerHTML = price;
+        flag22 = 1;
+    } else {
+        price -= 350;
+        tp.innerHTML = price;
+        flag22 = 0;
+    }
+});
+
+let flag23 = 0;
+item23.addEventListener("click", function () {
+    if (flag23 === 0) {
+        price += 850;
+        tp.innerHTML = price;
+        flag23 = 1;
+    } else {
+        price -= 850;
+        tp.innerHTML = price;
+        flag23 = 0;
+    }
+});
+
+let flag24 = 0;
+item24.addEventListener("click", function () {
+    if (flag24 === 0) {
+        price += 150;
+        tp.innerHTML = price;
+        flag24 = 1;
+    } else {
+        price -= 150;
+        tp.innerHTML = price;
+        flag24 = 0;
+    }
+});
+let flag25 = 0;
+item25.addEventListener("click", function () {
+    if (flag25 === 0) {
+        price += 1200;
+        tp.innerHTML = price;
+        flag25 = 1;
+    } else {
+        price -= 1200;
+        tp.innerHTML = price;
+        flag25 = 0;
+    }
+});
+
+let flag26 = 0;
+item26.addEventListener("click", function () {
+    if (flag26 === 0) {
+        price += 850;
+        tp.innerHTML = price;
+        flag26 = 1;
+    } else {
+        price -= 850;
+        tp.innerHTML = price;
+        flag26 = 0;
+    }
+});
+
+let flag27 = 0;
+item27.addEventListener("click", function () {
+    if (flag27 === 0) {
+        price += 250;
+        tp.innerHTML = price;
+        flag27 = 1;
+    } else {
+        price -= 250;
+        tp.innerHTML = price;
+        flag27 = 0;
+    }
+});
+
+let flag28 = 0;
+item28.addEventListener("click", function () {
+    if (flag28 === 0) {
+        price += 700;
+        tp.innerHTML = price;
+        flag28 = 1;
+    } else {
+        price -= 700;
+        tp.innerHTML = price;
+        flag28 = 0;
+    }
+});
